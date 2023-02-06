a5c9b37931c90f0c60af954f980b99269e7b7949
Cláudio Daniel <cl_daniel101@claudio-daniel.SAMSUNG>
2023-02-06 16:46:09 -0300

    Retirando comentários

--- videos/validators.py
+++ videos/validators.py
@@ -6,9 +6,3 @@ def valida_url(url):
     return resposta
 
 print(valida_url('https://www.youtube.com/watch?v=-PUy9AEM72Q'))
-
-    # https://www.youtube.com/watch?v=_loRrBA9EpY&ab_channel=PauloJubilut
-
-    # modelo = '[\(]?[0-9]{2}[\)]?[ ]?(9)?[0-9]{4}[\-| ]?[0-9]{4}'
-    # resposta = re.findall(modelo, celular)
-    # return resposta
