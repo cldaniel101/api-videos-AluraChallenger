add739d081b83617bcea9193a8e55d0f6b59438c
Claudio Daniel <danielperuna2012@gmail.com>
2023-02-07 17:04:46 -0300

    Ajeitando o Validators

--- videos/validators.py
+++ videos/validators.py
@@ -4,5 +4,3 @@ def valida_url(url):
     modelo = "(http://|https://)?(www\.)?\w+\.com(\.\w{3})?/[a-z]*\?v=.+"
     resposta = re.match(modelo, url)
     return resposta
-
-print(valida_url('https://www.youtube.com/watch?v=-PUy9AEM72Q'))
